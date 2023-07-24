d01b2c7110504ba0fb84a6f865fed424bfb5912a
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2023-07-25 01:51:02 +0300

    25/7/2023 ebrahim created firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:manaber/shared/network/local/shared_preferences.dart';
 import 'package:manaber/shared/network/remote/dio_helper.dart';
 import 'features/splash/view.dart';
 import 'shared/styles/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await DioHelper.init();
   await CacheHelper.init();
   runApp(const MyApp());
